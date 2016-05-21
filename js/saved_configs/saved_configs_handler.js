d7652c9c70d525fefa4f0fb00efa4acb7298e697
ljacqu <ljacqu@users.noreply.github.com>
2016-05-21 13:02:15 +0200

    Saved configurations - allow updating and config reset

--- js/saved_configs/saved_configs_handler.js
+++ js/saved_configs/saved_configs_handler.js
@@ -6,38 +6,70 @@ $(function () {
     var CONFIGS_SELECT = '.js-config-select';
     var SHOW_CHECKBOX = '.js-show-checkbox';
     var DELETE_CONFIG_BTN = '.js-config-deletebtn';
+    var UPDATE_CONFIG_BTN = '.js-config-updatebtn';
+    var REAPPLY_CONFIG_BTN = '.js-config-reapplybtn';
 
     var createOptionTag = function (id, name) {
         return '<option value="' + id + '">' + name + '</option>';
     };
 
-    var saveNewConfig = function () {
-        var name = window.prompt('Enter name of config to save');
-        if (name === null) {
-            return; // prompt was canceled
+    var updateActionButtonsVisibility = function (selectedItemValue) {
+        var actionButtons = $(UPDATE_CONFIG_BTN + ', ' + DELETE_CONFIG_BTN + ', ' + REAPPLY_CONFIG_BTN);
+        if (selectedItemValue === 'create' || selectedItemValue === 'blank') {
+            actionButtons.hide();
+        } else {
+            actionButtons.show();
+        }
+    };
+
+    var selectItemInDropdown = function (itemValue) {
+        var dropdown = $(CONFIGS_SELECT);
+        dropdown.find(':selected').prop('selected', false);
+        dropdown.find('[value="' + itemValue + '"]').prop('selected', true);
+        updateActionButtonsVisibility(itemValue);
+    };
+
+    var saveConfig = function (id) {
+        var config = {};
+        if (id) {
+            config.id = id;
+            config.name = $(CONFIGS_SELECT).find(':checked').text();
+        } else {
+            var name = window.prompt('Enter name of config to save');
+            if (name === null) {
+                return; // prompt was canceled
+            }
+            config.name = name;
         }
         var selectedShowIds = $(SHOW_CHECKBOX).filter(':checked')
             .map(function () { return $(this).val(); }).get();
-        var config = {
+        $.extend(config, {
             shows: selectedShowIds,
-            name: name,
             fileName: $('input[name="file"]').val(),
             threshold: $('input[name="threshold"]').val(),
             type: $('input[name="type"]:checked').val(),
             unit: $('input[name="unit"]:checked').val()
-        };
+        });
 
         $.post('./js/saved_configs/edit_config.php', {config: config}, function (data) {
             configs[data.id] = config;
-            $(CONFIGS_SELECT).append(createOptionTag(data.id, config.name));
+            if (!id) {
+                $(CONFIGS_SELECT).append(createOptionTag(data.id, config.name));
+                selectItemInDropdown(data.id);
+            }
         });
     };
 
+    var updateConfig = function () {
+        saveConfig($(CONFIGS_SELECT).find(':selected').val());
+    };
+
     var updateShowCheckboxes = function (showIds) {
-        $('.js-show-checkbox:checked').prop('checked', false);
         $.each($(SHOW_CHECKBOX), function () {
             if ($.inArray($(this).val(), showIds) !== -1) {
                 $(this).prop('checked', true);
+            } else {
+                $(this).prop('checked', false);
             }
         });
     };
@@ -72,24 +104,28 @@ $(function () {
         $.post('./js/saved_configs/delete_config.php', {id: showId}, function () {
             selectedOption.remove();
             delete configs[showId];
+            selectItemInDropdown('blank');
         });
     };
 
     var handleSelectChange = function () {
-        var deleteButton = $(DELETE_CONFIG_BTN);
+        updateActionButtonsVisibility($(this).val());
         if ($(this).val() === 'create') {
-            deleteButton.hide();
-            saveNewConfig();
+            saveConfig();
         } else if ($(this).val() !== 'blank') {
             applyConfig($(this).val());
-            deleteButton.show();
         }
+    };
+
+    var applySelectedConfig = function () {
+        var configId = $(CONFIGS_SELECT).find(':selected').val();
+        applyConfig(configId);
     };
 
     /**
      * Populates the set element for configs with the available configurations.
      */
-    var populateDropdown = function () {
+    var buildDropdownContent = function () {
         var selectElem = $(CONFIGS_SELECT);
         var result = '<option value="blank" selected="selected">Select&hellip;</option>' +
             '<option value="create" style="border-bottom: 2px solid #fc9; font-style: italic">Create</option>';
@@ -105,7 +141,9 @@ $(function () {
     // Execution
     $.get('./js/saved_configs/get_configs.php', function (data) {
         configs = data;
-        populateDropdown();
+        buildDropdownContent();
+        $(REAPPLY_CONFIG_BTN).click(applySelectedConfig);
+        $(UPDATE_CONFIG_BTN).click(updateConfig);
         $(DELETE_CONFIG_BTN).click(deleteConfig);
     });
 
